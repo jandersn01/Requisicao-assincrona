44ae488677174435025e76acef607bacd334fdf7
Jandersn01 <jandersonsantos030@gmail.com>
2025-02-11 23:29:09 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const cepInput = document.getElementById('cep')
+cepIput.addEventListener('input', function(){
+    const cep = cepIput.value.replace(/\D/g, '')
+
+    if(cep === 8){
+        
+    }
+})
