3374620874d763a2c5a0b1b7a2c0dbd772661c6b
Jandersn01 <jandersonsantos030@gmail.com>
2025-02-17 23:10:57 -0300

    getCep() is done!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-const cepInput = document.getElementById('cep')
-cepIput.addEventListener('input', function(){
-    const cep = cepIput.value.replace(/\D/g, '')
 
-    if(cep === 8){
-        
-    }
+const cepInput = document.getElementById('cep')
+cepInput.addEventListener('input',()=>{
+    const cep = cepInput.value;
+        getCep(cep);
 })
+
+async function getCep(data){
+    const cep = data.replace(/\D/g, '')
+    try{
+        const resource = `https://viacep.com.br/ws/${cep}/json/`;
+
+        const res = await fetch(resource);
+        const json = await res.json();
+        if(json.erro){
+            ativarErro();
+            //limpar();
+        }
+        else{
+            montar(json)
+        }
+       
+        return  
+    }
+    catch(erro){
+        console.log(erro)
+        ativarErro();
+        //limpar()
+    }
+    
+}
+
+function montar(obj) {
+    desativarErro()
+    document.getElementById('street').value = obj.logradouro || "";
+    document.getElementById('neighborhood').value = obj.bairro || "";
+    document.getElementById('state').value = obj.uf || "";
+    document.getElementById('city').value = obj.localidade || "";
+}
+
+function desativarErro(){
+    const err = document.getElementById('cepError')
+    err.style.display = 'none';
+}
+function ativarErro(){
+    const err = document.getElementById('cepError')
+    err.style.display = 'block';
+}
+
+function limpar(){
+    document.querySelector('input').value = "";
+}
+
+   
